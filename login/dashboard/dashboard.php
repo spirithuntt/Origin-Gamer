2380d06bf95f00570e442341c5afa672061e5e42
nouhaila el housni <112879456+spirithuntt@users.noreply.github.com>
2022-11-17 02:10:30 +0100

    dashboard front-end
    dashboard front-end completed

---
+++ login/dashboard/dashboard.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+include 'component.html';
+?>
+
+
+
+<div class="col-sm-8">
+<h1 class="container page-header p-4">
+						Products table
+					</h1>
+  <div class="tableau w-75 m-5 w-100">
+        <table class="table table-hover" id=productstable >
+  <thead>
+    <tr>
+      <th scope="col">Product ID</th>
+      <th scope="col">Product Name</th>
+      <th scope="col">Brand</th>
+      <th scope="col">Category</th>
+      <th scope="col">Stock</th>
+      <th scope="col">Price</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Intel Core i5-13600KF</td>
+      <td>Intel</td>
+      <td>CPU</td>
+      <td>3</td>
+      <td>3090 DH</td>
+    </tr>
+    <tr>
+      <th scope="row">1</th>
+      <td>Intel Core i5-13600KF</td>
+      <td>Intel</td>
+      <td>CPU</td>
+      <td>10</td>
+      <td>3050 DH</td>
+    </tr>
+    <tr>
+      <th scope="row">1</th>
+      <td>Intel Core i5-13600KF</td>
+      <td>Intel</td>
+      <td>CPU</td>
+      <td>8</td>
+      <td>3065 DH</td>
+    </tr>
+  </tbody>
+</table>
+			</div>
+        <!-- BEGIN #table -->
+            
+</div>
+        <!-- END #table -->
+</div>
+        <!-- END #table -->
+	</div>
+  </main>
+
+</body>
+</html>
---
+++ login/dashboard/profile.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+include 'component.html';
+?>
+
+<div class="col-sm-10">
+<div class="container p-5 row">
+<div class="row g-6 mb-6">
+                    <div class="col-xl-4 col-sm-6 col-12 my-2">
+                        <div class="card shadow border-warning">
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col">
+                                        <span class="h6 font-semibold text-muted text-sm d-block mb-2">Products</span>
+                                        <span class="h3 font-bold mb-0">80</span>
+                                    </div>
+                                    <div class="col-auto">
+                                        <div class="icon icon-shape bg-tertiary text-white text-lg rounded-circle">
+                                            <i class="bi bi-credit-card"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="mt-2 mb-0 text-sm">
+                                    <span class="badge badge-pill bg-soft-success text-success me-2">
+                                        <i class="bi bi-arrow-up me-1"></i>13%
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-4 col-sm-6 col-12 my-2 ">
+                        <div class="card shadow border-success ">
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col">
+                                        <span class="h6 font-semibold text-muted text-sm d-block mb-2">Profit</span>
+                                        <span class="h3 font-bold mb-0">215DH</span>
+                                    </div>
+                                    <div class="col-auto">
+                                        <div class="icon icon-shape bg-primary text-white text-lg rounded-circle">
+                                            <!-- <i class="bi bi-people"></i> -->
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="mt-2 mb-0 text-sm my-2">
+                                    <span class="badge badge-pill bg-soft-success text-success me-2">
+                                        <i class="bi bi-arrow-up me-1"></i>30%
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-4 col-sm-6 col-12 my-2">
+                        <div class="card shadow border-info">
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col">
+                                        <span class="h6 font-semibold text-muted text-sm d-block mb-2">Spendings</span>
+                                        <span class="h3 font-bold mb-0">1.400DH</span>
+                                    </div>
+                                    <div class="col-auto">
+                                        <div class="icon icon-shape bg-info text-white text-lg rounded-circle">
+                                            <!-- <i class="bi bi-clock-history"></i> -->
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="mt-2 mb-0 text-sm">
+                                    <span class="badge badge-pill bg-soft-danger text-danger me-2">
+                                        <i class="bi bi-arrow-down me-1"></i>-5%
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                </div>
+                </div>
+
+</body>
+</html>
